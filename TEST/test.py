3a9e07ad4271392118803b2a033b5bce018886bf
dvdbnz <learner-e27a9d@extensionschool.ch>
2020-10-04 10:51:10 +0200

    creation of the class + creation of the method to convert class as dict and add it to the CSV.

--- TEST/test.py
+++ TEST/test.py
@@ -1,15 +1,35 @@
 
 import csv
 
-fields_name = ['client','TV A','TV B']
+# fields_name = ['client','TV A','TV B']
 
-list = {"client": "blabla",
-        'TV A' : 5 ,
-        'TV B': 1000}
+# list = {"client": "blabla",
+#         'TV A' : 5 ,
+#         'TV B': 1000}
 
-def add_new_line_to_csv(new_line):
-    with open(r'C:\Users\davbu\OneDrive\Dokumente\Learning\TCC\Final Project\TEST\database.csv', 'a') as file:
-        writer = csv.DictWriter(file, fieldnames=fields_name)
-        writer.writerow(new_line)
+# def add_new_line_to_csv(new_line):
+#     with open(r'C:\Users\davbu\OneDrive\Dokumente\Learning\TCC\Final Project\TEST\database.csv', 'a') as file:
+#         writer = csv.DictWriter(file, fieldnames=fields_name)
+#         writer.writerow(new_line)
 
-add_new_line_to_csv(list)
+# add_new_line_to_csv(list)
+
+
+# Essayer une fois de faire une classe, de la créer et de pushed les données dans le CSV
+class Shop:
+        def __init__ (self, client, tv_a, tv_b):
+                self.client = client
+                self.tv_a = tv_a
+                self.tv_b = tv_b
+        def add_new_line_to_csv(self):
+                fields_name = ['client','tv_a','tv_b']
+                list2 = self.__dict__
+                # list = {"client": "blabla", 'tv_a' : 5 ,'tv_b': 1000}
+                with open(r'C:\Users\davbu\OneDrive\Dokumente\Learning\TCC\Final Project\TEST\database.csv', 'a', newline='') as file:
+                        writer = csv.DictWriter(file, fieldnames=fields_name)
+                        writer.writerow(list2)
+                # print(list)
+                print(list2)
+
+client = Shop("blabla",11,11)
+client.add_new_line_to_csv()
--- program.py
+++ program.py
@@ -1,14 +1,51 @@
 import csv
 import flask
+from flask import Flask, request, redirect
+import cgi
 app = flask.Flask("Reporting Tool")
 
+def get_html(page_name):
+    html_file = open(page_name + ".html")
+    content = html_file.read()
+    html_file.close()
+    return content
+
+# creation of the Shop class
 class Shop:
-    def __init__ (self, name, tv_a, tv_b, tv_c):
-        self.name = name
-        self.tv_a = tv_a
-        self.tv_b = tv_b
-        self.tv_c = tv_c
+    def __init__ (self, client, lg_oled, sony_oled):
+        self.client = client
+        self.lg_oled = lg_oled
+        self.sony_oled = sony_oled
+    # Convert object as dict to add it to the csv
+    def add_new_line_to_csv(self):
+            fields_name = ['client','lg_oled','sony_oled']
+            list = self.__dict__
+            with open(r'C:\Users\davbu\OneDrive\Dokumente\Learning\TCC\Final Project\database.csv', 'a', newline='') as file:
+                    writer = csv.DictWriter(file, fieldnames=fields_name)
+                    writer.writerow(list)
+            print(list)
+
 
 @app.route("/")
 def homepage():
-    return "HOMEPAGE"
+    page = get_html("index")
+    return page
+
+@app.route('/survey')
+def survey():
+    page = get_html('survey')
+    return page
+
+
+#When the user clicks on submit the data from the survey, it gets the values from the survey.html
+#Then uses them as arguments to create an object. Then append the object as Dict in the Csv
+# Then redirect to the homepage
+@app.route('/survey', methods=['POST'])
+def my_form_post():
+    client = request.form['client']
+    lg_oled = request.form['lg-oled']
+    sony_oled = request.form['sony-oled']
+    NewClient = Shop(client,lg_oled,sony_oled)
+    NewClient.add_new_line_to_csv()
+    return redirect('/')
+    
